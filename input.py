7d4fe2258b772b8ca508c7eecaac7046fee07174
Andrew Rowley <asrowley@hotmail.com>
2016-08-05 11:22:28 +0100

    Create input.py

---
+++ input.py
@@ -0,0 +1,7 @@
+#you can input information into the computer which it can remember
+
+my_name = input("What is your name?")
+
+print(my_name)
+
+#write code below which asks for the user's favourite colour then prints it.
